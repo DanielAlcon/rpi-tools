522c3afe3578d9a73677315e6eed202421845f2d
Daniel <daniel@pc-daniel>
2023-02-17 17:50:10 +0100

    revert to basics

--- checkwifi.sh
+++ checkwifi.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 # you need to sudo chmod 775 the file and then in crontab */5 * * * * /usr/bin/sudo -H /path/to/your/checkwifi.sh >> /dev/null 2>&1
-COUNTER=0
-for OUTPUT in $(seq 5)
-do
-    let COUNTER++
-    ping -c4 www.google.es > /dev/null
-    if [ [$? != 0] && [$COUNTER -gt 5] ] 
-    then
-        echo "No network connection, restarting raspberry"
-        sudo /sbin/shutdown -r now
-    else
-        echo "No network connection, restarting wlan0"
-        /sbin/ifdown 'wlan0'
-        sleep 5
-        /sbin/ifup --force 'wlan0'
-    fi
-done
+ping -c4 www.google.es > /dev/null
+ 
+if [ $? != 0 ] 
+then
+  sudo /sbin/shutdown -r now
+fi
