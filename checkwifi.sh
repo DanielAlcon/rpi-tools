f769274b954dbdf15458b36012f13e1797a62258
Daniel <daniel@pc-daniel>
2023-02-17 17:44:37 +0100

    change of order

--- checkwifi.sh
+++ checkwifi.sh
@@ -7,11 +7,12 @@ do
     ping -c4 www.google.es > /dev/null
     if [ [$? != 0] && [$COUNTER -gt 5] ] 
     then
+        echo "No network connection, restarting raspberry"
+        sudo /sbin/shutdown -r now
+    else
         echo "No network connection, restarting wlan0"
         /sbin/ifdown 'wlan0'
         sleep 5
         /sbin/ifup --force 'wlan0'
-    else
-        sudo /sbin/shutdown -r now
     fi
 done
